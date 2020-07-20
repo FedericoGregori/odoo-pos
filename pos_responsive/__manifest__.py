e0869df487307e8c486a3b2eee7fc2fa1a1a6700
FedericoGregori <jfedericogregori@gmail.com>
2020-07-20 02:14:56 -0300

    [ADD][pos_responsive] Initial

---
+++ pos_responsive/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Responsive for PoS",
+
+    'summary': "Mobile view for PoS screen.",
+
+    'author': "Federico Gregori",
+    'license': "AGPL-3",
+    'website': "www.federicogregori.com",
+    'category': 'PoS',
+    'version': '13.0.0.1',
+    'depends': ['point_of_sale', 'pos_restaurant'],
+    'qweb': ['static/src/xml/pos.xml'],
+    'data': ['views/pos_responsive.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
